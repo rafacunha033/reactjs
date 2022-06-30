1ca2ecc6bc1e80fc2962ee6581c9df307be6781a
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-06-30 16:31:02 +0000

    Creation of components and some styling with tailwind (header, lesson, videoo and sidebar)

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,62 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react"
-import { client } from "./lib/apollo";
-
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-      }
-    }
-  }
-`
-
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY)
-
   return (
-    <ul>
-      {data?.lessons.map((lesson: Lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div className="flex flex-col min-h-screen">
+      <Header />
 
+      <main className="flex flex-1">
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+// import { gql, useQuery } from "@apollo/client";
+// import { useEffect } from "react"
+// import { client } from "./lib/apollo";
+
+
+// const GET_LESSONS_QUERY = gql`
+//   query {
+//     lessons {
+//       id
+//       title
+
+//       teacher {
+//         name
+//       }
+//     }
+//   }
+// `
+
+// interface Lesson {
+//   id: string;
+//   title: string;
+// }
+
+// function App() {
+//   const { data } = useQuery(GET_LESSONS_QUERY)
+
+//   return (
+//     <ul>
+//       {data?.lessons.map((lesson: Lesson) => {
+//         return <li key={lesson.id}>{lesson.title}</li>
+//       })}
+//     </ul>
+
+//   )
+// }
+
+// export default App
